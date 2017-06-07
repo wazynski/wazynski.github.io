2bab56183d05b6b80ef30462858c0239bb6c3de3
Tom Skarbek Wazynski <t.skarbek-wazynski@lancaster.ac.uk>
2017-06-07 14:38:26 +0100

    Started styling
    Signed-off-by: Tom Skarbek Wazynski <t.skarbek-wazynski@lancaster.ac.uk>

--- js/main.js
+++ js/main.js
@@ -4,10 +4,50 @@ var nine = {};
   cgts.homepageSnap
 ========================================================================== */
 
-nine.paralax = function() {
-  console.log(document.querySelectorAll('.paralax'));
-};
+(function() {
+  // 'use strict';
+
+  var section = document.querySelectorAll(".section");
+  var sections = {};
+  var i = 0;
+
+  console.log(section);
+
+  Array.prototype.forEach.call(section, function(el, i) {
+    console.log(el.className);
+    console.log(el.offsetTop);
+    sections[i] = {
+      classes: el.className.replace('section', '').trim(),
+      top: el.offsetTop,
+    }
+  });
+
+  console.log(sections);
+
+  window.onscroll = function() {
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (i in sections) {
+      if (sections[i].top <= scrollPosition) {
+
+
+        if (sections[i].classes.includes('light')) {
+          console.log('menu dark');
+          document.querySelector('.header').setAttribute('class', 'header dark');
+        } else {
+          console.log('menu light');
+          document.querySelector('.header').setAttribute('class', 'header light');
+        }
+        //string.includes(substring); //ES6
+
+        // document.querySelector('.active').setAttribute('class', ' ');
+        // document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+      }
+    }
+  };
+})();
+
 
 document.addEventListener("DOMContentLoaded", function() {
-    nine.paralax();
+
 });
